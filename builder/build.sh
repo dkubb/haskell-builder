b94abd0fb7fcdb8fd3e571b48270968d7af8841a
Dan Kubb <dan.kubb@gmail.com>
2015-07-30 00:30:07 -0700

    Add script for building docker images for statically linked binaries

---
+++ builder/build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -feuo pipefail
+IFS=$'\n\t'
+
+if [ $# -eq 0 ] || [ "$1" == '--help' ] || [ "$1" == '-h' ]; then
+  echo "Example:" \
+       "docker run" \
+       "-v \$(pwd):/src" \
+       "-v /var/run/docker.sock:/var/run/docker.sock" \
+       "<container>" \
+       "<package-name>" \
+       "[tag-name]"
+  exit 1
+fi
+
+package="$1"
+tag="${2-"$package":latest}"
+ghc_options=${ghc_options:--O2 -threaded -optl-static -optl-s}
+socket=/var/run/docker.sock
+file=Dockerfile
+
+echo "Building $package"
+stack build --ghc-options "$ghc_options" .
+
+if [ -S $socket -a -r $socket -a -w $socket -a -f $file -a -r $file ]; then
+  ln -snf "$(stack path --dist-dir)/build" .
+  docker build --tag "$tag" --file "$file" .
+  echo "Created container $tag"
+fi
